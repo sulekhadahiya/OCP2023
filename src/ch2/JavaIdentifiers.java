deeadd3ab82d949bc2c5cf633bb6ece52ab42f2a
sulekhadahiya <sulekhadahiya12@gmail.com>
2023-06-26 16:39:52 -0400

    ch-2 initial commit.

---
+++ src/ch2/JavaIdentifiers.java
@@ -0,0 +1,30 @@
+package ch2;
+
+/*
+    Lexical Tokens : The low-level language elements like Identifiers, numbers, operators, special characters.
+
+    Identifiers : A name in a program is called an identifier. Are used to denote classes, methods, variables and labels.
+
+    Volatile : Volatile keyword is used to indicate that a variable's value may be modified by a multiple threads
+               simultaneously. It ensures that the variable is always read from and written to the main memory,
+               rather than from thread-specific cache.
+
+    Serialization : Serialization in java is the concept of representing an object's state as a byte stream. The byte
+                    stream has all the information about the object. Serialization in java helps transport the code
+                    from one JVM to another and then de-serialize it there.
+
+    Transient : Transient is a java keyword which marks a member variable not to be serialized when it is persisted
+                to streams of bytes.
+
+    Separators : are like { , } , [ , ] , ( , ) , . , ; , , , ... , @ , :: .
+
+    Literals : A literal denotes a constant value; that means the value that a literal represents remain unchanged
+               in the program. Literal represent numerical (integer and floating-point), character, boolean, and
+               string values. In addition, the literal null represents the null reference.
+
+
+ */
+public class JavaIdentifiers {
+
+
+}
